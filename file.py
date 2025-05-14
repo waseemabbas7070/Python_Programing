f25395b08cfe879fed62548ce36c85b111e3cd03
abc <abc@gmail.com>
2025-05-14 12:43:33 +0500

    updated

--- file.py
+++ file.py
@@ -1,5 +1,22 @@
 import csv
-with open('weatherAUS.csv', mode ='r') as file:
-    reader = csv.reader(file)
-    for row in reader:
-        print(row)
+
+# Define data
+data = [
+    ['name', 'Age', 'City'],
+    ['Waseem', 24, 'Lahore'],
+    ['Nadeed', 20, 'Sahiwal']
+]
+
+# Write to data.csv
+with open('data.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerows(data)  # <-- FIXED here
+
+# Print specific item
+print(data)  # Output: Age
+
+# Read and print from info.csv
+# with open('info.csv', mode='r') as file:
+#     reader = csv.reader(file)
+#     for row in reader:
+#         print(row)
