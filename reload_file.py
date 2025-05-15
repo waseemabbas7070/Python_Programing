4be28c499c088ca5e07c14f64a88b67968a09c9c
abc <abc@gmail.com>
2025-05-15 10:25:33 +0500

    updated

--- reload_file.py
+++ reload_file.py
@@ -1,3 +1,4 @@
-import pandas as pd
-df = pd.read_excel('data.xlsx')
-print(df)
+import pandas as pd         # Importing pandas
+import openpyxl             # (Optional) Explicitly importing openpyxl
+df = pd.read_excel('data.xlsx', engine='openpyxl')  # Reading Excel file with specified engine
+print(df)                   # Displaying the DataFrame
